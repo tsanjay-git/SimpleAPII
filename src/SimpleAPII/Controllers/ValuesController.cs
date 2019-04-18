e2bf97de328184aa34bb43ee16a32cc26f09c14a
Sanjay <sanju4@gmail.com>
2019-04-18 14:31:49 +0530

    Testing release pipeline

--- src/SimpleAPII/Controllers/ValuesController.cs
+++ src/SimpleAPII/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPII.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Genpact", "EA" };
         }
 
         // GET api/values/5
